0b01875058ff09f07b69ff5ca3308efc55572fb2
Juan-Avila92 <juanavila1992@hotmail.com>
2020-11-09 18:08:12 -0500

    Roulette Business Class - It executes everything to do with Roulettes

---
+++ Roulette/RouletteBusiness/RouletteBusiness.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Roulette.Models;
+
+namespace Roulette.RouletteBusiness
+{
+    public static class RouletteBusiness
+    {
+        public static Roulette GetRouletteById(RouletteDbContext context, int id)
+        {
+            return context.Roulettes.FirstOrDefault(roulette => roulette.Id == id);
+        }
+        public static NoContentResult UpdateRouletteStateIfExists(RouletteDbContext context, Roulette rouletteToBeUpdated)
+        {
+            if(rouletteToBeUpdated != null) {
+                rouletteToBeUpdated.IsOpen = true;
+                context.Roulettes.Update(rouletteToBeUpdated);
+                context.SaveChanges();
+                return new NoContentResult();
+            }
+            return null;
+        }
+    }
+}
