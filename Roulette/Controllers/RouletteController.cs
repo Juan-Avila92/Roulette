cf0c6e60c3c3f1f02b36da5405568363bb30c8da
Juan-Avila92 <juanavila1992@hotmail.com>
2020-11-09 17:51:17 -0500

    name changes - contextDb is cleaner

--- Roulette/Controllers/RouletteController.cs
+++ Roulette/Controllers/RouletteController.cs
@@ -9,32 +9,48 @@ using Roulette.Models;
 namespace Roulette.Controllers
 {
     [ApiController]
-    [Route("api/roulettes")]
+    
     public class RouletteController : ControllerBase
     {
-        private readonly RouletteDbContext _context;
+        private readonly RouletteDbContext contextDb;
 
         public RouletteController(RouletteDbContext context)
         {
-            _context = context;
+            contextDb = context;
         }
 
         [HttpGet]
+        [Route("game/roulettes")]
         public List<Roulette> Get()
         {
-            return _context.Roulettes.ToList();
+            return contextDb.Roulettes.ToList();
         }
 
         [HttpPost]
+        [Route("game/roulette")]
         public IActionResult PostRoulette([FromBody] Roulette roulette)
         {
             if (!this.ModelState.IsValid)
             {
                 return BadRequest();
             }
-            this._context.Roulettes.Add(roulette);
-            this._context.SaveChanges();
+            this.contextDb.Roulettes.Add(roulette);
+            this.contextDb.SaveChanges();
             return Created($"roulettes/{roulette.Id}", roulette);
+        }
+
+        [HttpPut]
+        [Route("game/openbet/roulette/{id}")]
+        public string PutAuthors(int id)
+        {
+            Roulette rouletteToBeUpdated = RouletteBusiness.RouletteBusiness.GetRouletteById(contextDb, id);
+            NoContentResult result = RouletteBusiness.RouletteBusiness.UpdateRouletteStateIfExists(contextDb, rouletteToBeUpdated);
+            if (result != null) {
+                return string.Format("Roulette with Id: {0} has been opened.", id);
+            }
+            else {
+                return string.Format("There is no roulette with Id: {0} .", id); ;
+            }
         }
     }
 }
