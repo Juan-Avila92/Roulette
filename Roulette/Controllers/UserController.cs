702048e8551811d3efb5c86b072dab9237347973
Juan-Avila92 <juanavila1992@hotmail.com>
2020-11-09 17:50:28 -0500

    User Controller - Executes http methods

---
+++ Roulette/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Roulette.Models;
+
+namespace Roulette.Controllers
+{
+    public class UserController
+    {
+        private readonly RouletteDbContext contextDb;
+
+        public UserController(RouletteDbContext context)
+        {
+            contextDb = context;
+        }
+
+        [HttpGet]
+        [Route("game/roulettes")]
+        public List<Roulette> Get()
+        {
+            return contextDb.Roulettes.ToList();
+        }
+    }
+}
