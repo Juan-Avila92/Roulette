cb49ea9d9a416afbaf02d842cc469eaee15c1423
Juan-Avila92 <juanavila1992@hotmail.com>
2020-11-09 17:49:50 -0500

    User Model

---
+++ Roulette/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Roulette.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public bool IdRoulette { get; set; }
+        public string Credit { get; set; }
+    }
+}
