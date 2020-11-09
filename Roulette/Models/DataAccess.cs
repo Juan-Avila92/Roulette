1ecda868ac5c9705fe5301ea9eb477e14c78b3b3
Juan-Avila92 <juanavila1992@hotmail.com>
2020-11-09 17:52:19 -0500

    No spaces between lines

--- Roulette/Models/DataAccess.cs
+++ Roulette/Models/DataAccess.cs
@@ -1,11 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 
 namespace Roulette.Models{
-
     public class RouletteDbContext : DbContext{
     public RouletteDbContext(DbContextOptions<RouletteDbContext> data)
-    :base (data){}
-
+        :base (data){}
     public DbSet<Roulette> Roulettes{get; set;}
-}
+    }
 }
