166b90a9eaa84e622beab2b529cb7287843a51cb
Juan-Avila1092 <Juan-Avila1092>
2020-11-09 06:28:07 -0500

    Data Access - database connection

---
+++ Roulette/Models/DataAccess.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Roulette.Models{
+
+    public class RouletteDbContext : DbContext{
+    public RouletteDbContext(DbContextOptions<RouletteDbContext> data)
+    :base (data){}
+
+    public DbSet<Roulette> Roulettes{get; set;}
+}
+}
