0a01ed136cbb4d8ccd4f90e9fd4d35f27941eb93
Juan-Avila92 <juanavila1992@hotmail.com>
2020-11-09 20:39:31 -0500

    Business Classes

---
+++ Roulette/Services/RouletteBusiness.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Roulette.Models;
+
+namespace Roulette.Services
+{
+    public static class RouletteBusiness
+    {
+        public static Roulette GetRouletteById(RouletteDbContext context, int id)
+        {
+            return context.Roulettes.ToList().FirstOrDefault(roulette => roulette.Id == id);
+        }
+        public static NoContentResult UpdateRouletteStateIfExists(RouletteDbContext context, Roulette rouletteToBeUpdated)
+        {
+            if(rouletteToBeUpdated != null) {
+                rouletteToBeUpdated.IsOpen = true;
+                context.Roulettes.Update(rouletteToBeUpdated);
+                context.SaveChanges();
+                return new NoContentResult();
+            }
+            return null;
+        }
+        public static int FindOpenRouletteId(RouletteDbContext context)
+        {
+            try {
+                return context.Roulettes.ToList<Roulette>().Find(x => x.IsOpen == true).Id;
+            } 
+            catch(Exception e) {
+                return 0;
+            }
+        }
+
+        public static Roulette PlayRoulette(Roulette roulette)
+        {
+            Random random = new Random();
+            roulette.BetResult = random.Next(0, 36);
+            return roulette;
+        }
+
+    }
+}
---
+++ Roulette/Services/UserBusiness.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Roulette.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Roulette.Services
+{
+    public static class UserBusiness
+    {
+        public static bool CanCreateAnUser(int IdRoulette)
+        {
+            if(IdRoulette != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool CanBet(User user)
+        {
+            if(double.Parse(user.Bet) <= double.Parse(user.Credit)) {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsNumberWithinRange(User user)
+        {
+            if(user.RouletteNumber >= 0 && user.RouletteNumber <= 36)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static User FindUserByRouletteId(RouletteDbContext context, int id)
+        {
+            try {
+                return context.Users.Where(user => user.IdRoulette == id).FirstOrDefault();
+            }
+            catch (Exception e) {
+                return null;
+            }
+        }
+    }
+}
