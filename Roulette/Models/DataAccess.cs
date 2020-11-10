5f0672f807656399edc3a58473be51fbef5beb04
Juan-Avila92 <juanavila1992@hotmail.com>
2020-11-09 20:39:05 -0500

    Users data base access.

--- Roulette/Models/DataAccess.cs
+++ Roulette/Models/DataAccess.cs
@@ -2,8 +2,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Roulette.Models{
     public class RouletteDbContext : DbContext{
-    public RouletteDbContext(DbContextOptions<RouletteDbContext> data)
+        public RouletteDbContext(DbContextOptions<RouletteDbContext> data)
         :base (data){}
-    public DbSet<Roulette> Roulettes{get; set;}
+        public DbSet<Roulette> Roulettes{get; set;}
+        public DbSet<User> Users { get; set; }
     }
 }
